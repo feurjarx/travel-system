1e06693cbebaf2dee62af5c118e9d7162937b576
Ivan-Rebro <ivan-rebro@yandex.ru>
2016-10-30 19:37:23 +0700

    Add Ajax (rooms and tours)

---
+++ ControlTravelAgencySystem/ControlTravelAgencySystem/Controllers/RoomsController.cs
@@ -0,0 +1,63 @@
+﻿using ControlTravelAgencySystem.Models;
+using ControlTravelAgencySystem.Models.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ControlTravelAgencySystem.Controllers
+{
+    /// <summary>
+    /// Контроллер номеров
+    /// </summary>
+    public class RoomsController : Controller
+    {
+        // Хранилище данных БД
+        private readonly TravelSystemEntities _dbContext;
+
+        /// <summary>
+        /// Инициализация
+        /// </summary>
+        public RoomsController(TravelSystemEntities dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Представление списка номеров выбранного отеля
+        /// </summary>
+        /// <param name="id">ID отеля</param>
+        public PartialViewResult GetRoomsList(int id)
+        {
+            var viewModel = GetRoomsViewModel(id);
+            return PartialView(viewModel);
+        }
+
+        /// <summary>
+        /// Возвращает модель-представление списка номеров
+        /// </summary>
+        public RoomsView GetRoomsViewModel(int id)
+        {
+            var viewModel = new RoomsView();
+
+            var rooms = _dbContext.rooms
+                .Include("hotel")
+                .Where(x => x.hotel_id == id);
+
+            foreach (var room in rooms)
+            {
+                viewModel.RoomViewItems.Add(
+                    new RoomsView.RoomViewItem
+                    {
+                        RoomId = room.id,
+                        RoomNumber = room.number,
+                        CostPerDay = room.cost_per_day,
+                        Class = room.@class,
+                        SeatsNumber = room.seats_number,
+                        RoomSize = room.room_size ?? 0,
+                        Description = room.description
+                    });
+            }
+
+            return viewModel;
+        }
+    }
+}
---
+++ ControlTravelAgencySystem/ControlTravelAgencySystem/Controllers/ToursController.cs
@@ -0,0 +1,54 @@
+﻿using ControlTravelAgencySystem.Models;
+using ControlTravelAgencySystem.Models.ViewModels;
+using System.Web.Mvc;
+
+namespace ControlTravelAgencySystem.Controllers
+{
+    /// <summary>
+    /// Контроллер курортов
+    /// </summary>
+    public class ToursController : Controller
+    {
+        // Хранилище данных БД
+        private readonly TravelSystemEntities _dbContext;
+
+        /// <summary>
+        /// Инициализация
+        /// </summary>
+        public ToursController(TravelSystemEntities dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// Представление списка туров
+        /// </summary>
+        public PartialViewResult GetToursList()
+        {
+            var viewModel = GetToursViewModel();
+            return PartialView(viewModel);
+        }
+
+        /// <summary>
+        /// Возвращает модель-представление списка туров
+        /// </summary>
+        private ToursView GetToursViewModel()
+        {
+            var viewModel = new ToursView();
+
+            var tours = _dbContext.tours
+                .Include("country");
+
+            foreach (var tour in tours)
+                viewModel.TourViewItems.Add(
+                    new ToursView.TourViewItem
+                    {
+                        TourId = tour.id,
+                        TourName = tour.name,
+                        CountryName = tour.country?.name
+                    });
+
+            return viewModel;
+        }
+    }
+}
