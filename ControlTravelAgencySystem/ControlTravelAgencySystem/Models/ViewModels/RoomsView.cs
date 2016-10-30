1c47779b6ed99df91d368e9bfe1ce2e12c5830c7
Ivan-Rebro <ivan-rebro@yandex.ru>
2016-10-30 22:41:19 +0700

    Add Buttons and FadeOut

---
+++ ControlTravelAgencySystem/ControlTravelAgencySystem/Models/ViewModels/RoomsView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlTravelAgencySystem.Models.ViewModels
+{
+    public class RoomsView
+    {
+        public class RoomViewItem
+        {
+            public int RoomId { get; set; }
+            public string RoomNumber { get; set; }
+            public int CostPerDay { get; set; }
+            public string Class { get; set; }
+            public int SeatsNumber { get; set; }
+            public int RoomSize { get; set; }
+            public string Description { get; set; }
+        }
+
+        public List<RoomViewItem> RoomViewItems { get; set; }
+
+        public RoomsView()
+        {
+            RoomViewItems = new List<RoomViewItem>();
+        }
+    }
+}
